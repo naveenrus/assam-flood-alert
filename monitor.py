6886c293483e126b911b2915076f169f1fab3cd9
naveenrus <bussarinaveen18@gmail.com>
2026-06-24 15:37:11 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -2,18 +2,31 @@ import os
 import smtplib
 from email.mime.text import MIMEText
 
-EMAIL_USER = os.environ["EMAIL_USER"]
-EMAIL_PASS = os.environ["EMAIL_PASS"]
-EMAIL_TO = os.environ["EMAIL_TO"]
+EMAIL_USER = os.environ["EMAIL_USER"].strip()
+EMAIL_PASS = os.environ["EMAIL_PASS"].strip()
 
-msg = MIMEText("GitHub Actions email test successful!")
+EMAIL_TO = [
+    x.strip()
+    for x in os.environ["EMAIL_TO"].split(",")
+]
+
+msg = MIMEText("GitHub Email Test Success")
 
 msg["Subject"] = "Assam Flood Alert Test"
 msg["From"] = EMAIL_USER
-msg["To"] = EMAIL_TO
+msg["To"] = ", ".join(EMAIL_TO)
 
 with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-    server.login(EMAIL_USER, EMAIL_PASS)
-    server.send_message(msg)
+
+    server.login(
+        EMAIL_USER,
+        EMAIL_PASS
+    )
+
+    server.sendmail(
+        EMAIL_USER,
+        EMAIL_TO,
+        msg.as_string()
+    )
 
 print("Email Sent Successfully")
